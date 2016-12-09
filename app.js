2e07c007a6121f7a7c4f3bfae1e6adc7ac6afa04
محمد عبد الناصر عبد العظيم <    >
2016-12-09 23:55:34 +0200

    No thing

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express() ;
+
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === '<Osboha_verify>') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
