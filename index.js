2880384f0c3366b05d1ac42725c9ec973e74901f
محمد عبد الناصر عبد العظيم <    >
2016-12-10 00:30:26 +0200

    try to make this work

--- index.js
+++ index.js
@@ -22,4 +22,6 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
