efb4ae975ec3efb7f692a73ba07d638993cbd85f
محمد عبد الناصر عبد العظيم <    >
2016-12-10 01:12:46 +0200

    pleese

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
-var app = express() ;
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.send('hello world');
-  console.console.log("Finaly");
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.send("<html> <h1> hey </h1> </html>");
+
 });
 
 app.get('/webhook', function(req, res) {
@@ -21,8 +27,4 @@ app.get('/webhook', function(req, res) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
 });
