89e1eb7536934ac5fa790b484e9d21eee51c866c
محمد عبد الناصر عبد العظيم <    >
2016-12-10 00:09:40 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express() ;
 
+app.set('port' , (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
   res.send('hello world');
@@ -15,4 +16,8 @@ app.get('/webhook', function(req, res) {
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);
   }
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
